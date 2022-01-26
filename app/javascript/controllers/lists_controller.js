ac191be94340a5cc51b376b5138dc0179dfdfc0b
Frank Alkema <frank@aeroscan.nl>
2022-01-26 21:53:20 +0100

    Made the modal work

--- app/javascript/controllers/lists_controller.js
+++ app/javascript/controllers/lists_controller.js
@@ -3,7 +3,30 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
 
-  greet() {
-    console.log("this works")
+  connect(){
+    console.log("lists javascript loaded")
+    // Get the modal
+    const modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    const btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
 }
