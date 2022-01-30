0d6c0ca3c8f9b4ec7d4ff163849b0e6d384d4dda
Frank Alkema <frank@aeroscan.nl>
2022-01-30 12:42:28 +0100

    More javascript refactoring

--- app/javascript/controllers/lists_controller.js
+++ app/javascript/controllers/lists_controller.js
@@ -1,21 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
+  static targets = ["addButton"]
 
   connect(){
     console.log("lists javascript loaded")
     // Get the modal
     const modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    const btn = document.getElementById("myBtn");
-
     // Get the <span> element that closes the modal
     const span = document.getElementsByClassName("close-modal")[0];
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function() {
+    this.addButtonTarget.onclick = function() {
       modal.style.display = "block";
     }
     // When the user clicks on <span> (x), close the modal
