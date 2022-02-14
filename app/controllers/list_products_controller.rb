e8155f9b17e7f28a42a9c12bd8cd14e5e4865b32
Frank Alkema <frank@aeroscan.nl>
2022-02-14 16:58:45 +0100

    Added a new crud ation to the list_products

--- app/controllers/list_products_controller.rb
+++ app/controllers/list_products_controller.rb
@@ -10,9 +10,14 @@ class ListProductsController < ApplicationController
     @list_product = ListProduct.create!(list_product_params)
   end
 
+  def update
+    @list_product = ListProduct.find(params[:id])
+    @list_products.update!(list_product_params)
+  end
+
   private
 
   def list_product_params
-    params.permit(:amount, :product_id, :list_id)
+    params.permit(:amount, :product_id, :list_id,:collected)
   end
 end
